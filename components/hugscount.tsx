b3abe4144badac2ef86174dd154b50e978f73e0c
Haider Ali Punjabi <me@haideralipunjabi.com>
2025-02-23 06:12:32 +0530

    fixes style

--- components/hugscount.tsx
+++ components/hugscount.tsx
@@ -1,24 +1,23 @@
 import { SupabaseClientHelper } from "lib/getSupabaseClient";
 export default async function HugsCount({ userDid }: { userDid: string }) {
-  const supabaseHelper = new SupabaseClientHelper();
-  await supabaseHelper.authorize();
-  return (
-    <>
-      <div className="text-center">
-        You have {await supabaseHelper.getRemainingFreeHugs(userDid)} free hugs
-        left today.
-      </div>
-      <div>
-        You have {await supabaseHelper.getRemainingPremiumHugs(userDid)} paid
-        hugs.{" "}
-        <a
-          href="https://haideralipunjabi.gumroad.com/l/epupp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Buy More
-        </a>
-      </div>
-    </>
-  );
+    const supabaseHelper = new SupabaseClientHelper();
+    await supabaseHelper.authorize();
+    return (
+        <>
+            <div className="text-center">
+                You have {await supabaseHelper.getRemainingFreeHugs(userDid)} free hugs
+                left today.
+                <br />
+                You have {await supabaseHelper.getRemainingPremiumHugs(userDid)} paid
+                hugs.{" "}
+                <a
+                    href="https://haideralipunjabi.gumroad.com/l/epupp"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Buy More
+                </a>
+            </div>
+        </>
+    );
 }
